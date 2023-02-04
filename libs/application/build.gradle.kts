bb68629f4db5506037c5c2b62a397986c3563d81
kdohyeon <dhkim04@trenbe.com>
2023-02-04 20:07:59 +0900

    [NO-JIRA]

--- libs/application/build.gradle.kts
+++
@@ -1,7 +0,0 @@
-dependencies {
-    implementation(project(":libs:adapter-http"))
-    implementation(project(":libs:adapter-persistence"))
-
-    implementation("org.springframework:spring-context") // annotation
-    implementation("org.springframework.data:spring-data-commons") // Pageable
-}
--- libs/application/src/main/java/sample/kdohyeon/blog/port/in/blog/SearchBlogUseCase.java
+++
@@ -1,8 +0,0 @@
-package sample.kdohyeon.blog.port.in.blog;
-
-import sample.kdohyeon.blog.port.in.blog.command.BlogSearchCommand;
-import sample.kdohyeon.blog.port.in.blog.response.BlogDto;
-
-public interface SearchBlogUseCase {
-    BlogDto search(BlogSearchCommand command);
-}
